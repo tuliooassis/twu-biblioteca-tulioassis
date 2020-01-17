c42891d21e4b0155b85056fabc296366e60ceb64
Túlio Assis <tulioassis@outlook.com>
2020-01-17 18:42:46 -0300

    Added Mockito and Library Test

--- src/test/java/com/twu/biblioteca/LibraryControllerTest.java
+++ src/test/java/com/twu/biblioteca/LibraryControllerTest.java
@@ -2,27 +2,35 @@ package com.twu.biblioteca;
 
 import com.twu.biblioteca.controller.LibraryController;
 import com.twu.biblioteca.repository.BooksRepository;
+import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 import static org.junit.Assert.assertEquals;
 
+
 public class LibraryControllerTest {
-    // Todo: mock
-    BooksRepository booksRepository = new BooksRepository();
-    LibraryController libraryController = new LibraryController(booksRepository);
+    BooksRepository mockedBooksRepository;
+    LibraryController libraryController;
+
+    @Before
+    public void setupClass() {
+        this.mockedBooksRepository = mock(BooksRepository.class);
+        this.libraryController = new LibraryController(mockedBooksRepository);
+    }
 
     @Test
     public void welcomeMessageShouldBeCorrect() {
         String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
-        assertEquals(welcomeMessage, libraryController.getWelcomeMessage());
+        assertEquals(welcomeMessage, this.libraryController.getWelcomeMessage());
     }
 
     @Test
-    public void booksStringsShouldBeLikeThePattern() {
-        String booksString = "Book:\tBook 1\tAuthor:\tAuthor 1\tPublished at:\t1997\n" +
-                "Book:\tBook 2\tAuthor:\tAuthor 1\tPublished at:\t2000\n" +
-                "Book:\tBook 3\tAuthor:\tAuthor 2\tPublished at:\t2012\n";
-
-        assertEquals(booksString, libraryController.getBooksString());
+    public void getBooksShouldBeCalled1Time() {
+        this.libraryController.getBooksString();
+        verify(this.mockedBooksRepository, times(1)).getBooks();
     }
 }
